a61dab5d87ee1c1e35ab7a71eab6211b4fea6163
Dennis Veselov <wesselloff@gmail.com>
2021-03-04 00:28:37 +0300

    ДЗ к третьему уроку сделано. Задания со звездочками подсмотрел на разборе ДЗ.

--- mainapp/views.py
+++ mainapp/views.py
@@ -18,7 +18,7 @@ def index(request):
     return render(request, 'mainapp/index.html', context)
 
 
-def products(request):
+def products(request, id=None):
     context = {
         'title': 'GeekShop - Каталог',
         'year': datetime.now(),
